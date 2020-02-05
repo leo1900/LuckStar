deeea865e52573ab5b9e1b50c4576cbe5a4d72f2
liusy <542214712@qq.com>
2020-02-05 10:43:48 +0800

    加载Lua

---
+++ Assets/Test/LuaLoaderTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuaLoaderTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.AddComponent<XLuaManager>();
+        XLuaManager.Instance.InitLuaEnv();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
